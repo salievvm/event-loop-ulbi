8e11668b4d00a592e0f82a1d8107ecc446ec5c78
salievvm_mac <salievvm@gmail.com>
2024-02-04 02:04:33 +0600

    event-loop

---
+++ promise.js
@@ -0,0 +1,60 @@
+const PENDING = 'pending';
+const REJECTED = 'rejected';
+const FULFILLED = 'fulfilled';
+
+class MyPromise {
+    constructor(executor) {
+        this.status = PENDING;
+        this.result = undefined;
+
+        const resolve = (result) => {
+            if (this.status === PENDING) {
+                this.result = result;
+                this.status = FULFILLED;
+            }
+        }
+
+        const reject = (error) => {
+            if (this.status === PENDING) {
+                this.result = error;
+                this.status = REJECTED;
+            }
+        }
+
+        try {
+            executor(resolve, reject);
+        } catch (error) {
+            resolve(error);
+        }
+    }
+
+    then(onFulfilled, onRejected) {
+        if (onFulfilled && this.status === FULFILLED) {
+            onFulfilled(this.result);
+        }
+
+        if (onRejected && this.status === REJECTED) {
+            onFulfilled(this.result);
+        }
+    }
+}
+
+console.log(1)
+const promise = new MyPromise((resolve, reject) => {
+    console.log(2)
+    // setTimeout(() => resolve(2), 1000);
+    // setTimeout(() => reject(3), 1000);
+    resolve(4);
+})
+
+promise.then(
+    (result) => {
+        console.log({ result });
+    },
+    () => { }
+);
+console.log(3);
+console.log(promise);
+setTimeout(() => {
+    console.log(promise);
+}, 1500)
